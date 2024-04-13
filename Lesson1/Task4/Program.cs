067f4efe3944d751471f37dc0090dc9fcc70e8d8
SemakinKaya <semakinkay@gmail.com>
2024-04-13 17:20:39 +0200

    add Lesson1:Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 5;
+int b = 14;
+int c = 3;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}if (d > max)
+{
+    max = d;
+}if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
