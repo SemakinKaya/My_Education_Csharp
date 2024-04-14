ea355e90d9e77e0ae0b77bab9c07c07cb9e0a5b3
SemakinKaya <semakinkay@gmail.com>
2024-04-14 17:42:06 +0200

    add Leson2:Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] arr = { 2, 3, 4, 5, 1, 6, 12, 18, 2, 3 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
