f89b8da35cfc1359a5f6725da12db1082a07281f
SemakinKaya <semakinkay@gmail.com>
2024-04-25 14:07:41 +0200

    add Lesson4:Task4

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string getLetters(string s) 
+{ 
+    string letters = "";
+    foreach(char c in s) 
+    {
+        if(char.IsLetter(c)) // Изменено на char.IsLetter для поддержки букв из разных алфавитов
+        {
+            letters += c; // Более компактная запись для добавления символа
+        }
+    }
+    return letters; 
+}
+
+string str = Console.ReadLine();
+string result = getLetters(str);
+Console.WriteLine(result);
